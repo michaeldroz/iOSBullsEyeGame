695118e87132a70a854907b30c49c8fb378e62ac
Michael Droz <michael.droz@tcc-mdroz.local>
2014-11-14 08:13:48 -0800

    Bulk commit.

--- BullsEye/ViewController.swift
+++ BullsEye/ViewController.swift
@@ -10,17 +10,49 @@ import UIKit
 
 class ViewController: UIViewController {
     
-    var currentValue: Int = 0
+    var currentValue = 0
     
-    var targetValue: Int = 0
+    var targetValue = 0
+    
+    var score = 0
+    
+    var round = 1
     
     @IBOutlet weak var slider: UISlider!
+    
+    @IBOutlet weak var targetLabel: UILabel!
+    
+    @IBOutlet weak var scoreLabel: UILabel!
+    
+    @IBOutlet weak var roundLabel: UILabel!
+    
+    
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        let thumbImageNormal = UIImage(named: "small_twit")
+        slider.setThumbImage(thumbImageNormal, forState: .Normal)
+        
+        let thumbImageHighlighted = UIImage(named: "small_twit")
+        slider.setThumbImage(thumbImageHighlighted, forState: .Highlighted)
+        
+        let insets = UIEdgeInsets(top: 0, left: 14, bottom: 0, right: 14)
+        
+        if let trackLeftImage = UIImage(named: "SliderTrackLeft") {
+            let trackLeftResizable =
+            trackLeftImage.resizableImageWithCapInsets(insets)
+            slider.setMinimumTrackImage(trackLeftResizable, forState: .Normal)
+        }
+        if let trackRightImage = UIImage(named: "SliderTrackRight") {
+            let trackRightResizable =
+            trackRightImage.resizableImageWithCapInsets(insets)
+            slider.setMaximumTrackImage(trackRightResizable, forState: .Normal)}
+        
         // Do any additional setup after loading the view, typically from a nib.
-        currentValue = lroundf(slider.value)
-        targetValue = 1 + Int(arc4random_uniform(100))
+        startNewRound()
+        updateLabels()
+        
     }
 
     override func didReceiveMemoryWarning() {
@@ -30,16 +62,47 @@ class ViewController: UIViewController {
 
 
     @IBAction func showAlert() {
-        let message = "The value of the slider is: \(currentValue)" + "\nThe target value is: \(targetValue)"
+            let difference = abs(targetValue - currentValue)
+            let points = 100 - difference
         
-        let alert = UIAlertController(title: "Bull's Eye!",
+            score += points
+        
+            round += 1
+        
+        if points == 100 {
+            let message = "BULL'S EYE!!!"
+            
+            let alert = UIAlertController(title: "Droz Game",
+                message: message, preferredStyle: .Alert)
+            
+            let action = UIAlertAction(title: "OK", style: .Default, handler: {action in self.startNewRound()
+                self.updateLabels()})
+            
+            alert.addAction(action)
+            
+            presentViewController(alert, animated: true, completion: nil)
+            
+            //startNewRound()
+            //updateLabels()
+            
+        } else {
+        
+        let message = "You scored \(points) points"
+        
+        let alert = UIAlertController(title: "Droz Game",
             message: message, preferredStyle: .Alert)
         
-        let action = UIAlertAction(title: "OK", style: .Default, handler: nil)
+            let action = UIAlertAction(title: "OK", style: .Default, handler: {action in self.startNewRound()
+                self.updateLabels()})
         
         alert.addAction(action)
         
         presentViewController(alert, animated: true, completion: nil)
+        
+        //startNewRound()
+        //updateLabels()
+            
+        }
     }
     
     @IBAction func sliderMoved(slider: UISlider) {
@@ -47,6 +110,33 @@ class ViewController: UIViewController {
         currentValue = lroundf(slider.value)
     }
     
+    func startNewRound() {
+        targetValue = 1 + Int(arc4random_uniform(100))
+        currentValue = 50
+        slider.value = Float(currentValue)
+    }
+    
+    @IBAction func startOver(){
+        
+        println("I am inside startOver()")
+    
+    currentValue = 0
+    
+     targetValue = 1 + Int(arc4random_uniform(100))
+    
+    score = 0
+    
+    round = 1
+        
+         updateLabels()
+
+    }
+    
+    func updateLabels() {
+        targetLabel.text = String(targetValue)
+        scoreLabel.text = String(score)
+        roundLabel.text = String(round)
+    }
     
 }
 
